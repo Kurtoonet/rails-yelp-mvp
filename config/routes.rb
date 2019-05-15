126f5f0e04bc326eda84c6e4de1bba9c484b5f60
Kurt Pascual <kurt.pascual84@gmail.com>
2019-05-15 17:22:19 +0200

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurants#destroy"
+
+  get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+  post "restaurants/:restaurant_id/reviews", to: "reviews#create"
+
+
+# Restaurant.new(params)
+
+#   /restaurants/12/reviews/24
+
+#   params = {
+#     restaurant_id: 12
+#     id: 24
+#   }
+
+  # get "restaurants/:restaurant_id/reviews",     to: "reviews#index"
+  # get "restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+  # post "restaurants/:restaurant_id/reviews",    to: "reviews#create"
+  # get "reviews/:id",                            to: "reviews#show"
+  # get "reviews/:id/edit",                       to: "reviews#edit"
+  # patch "reviews/:id",                          to: "reviews#update"
+  # delete "reviews/:id",                         to: "reviews#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+require 'faker'
+
+puts "we are detroying all the restaurants"
+Restaurant.destroy_all
+puts "all restaurant are destroyed"
+
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  puts " creating restaurants"
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: category.sample
+    )
+  restaurant.save!
+  puts "restaurants created"
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
